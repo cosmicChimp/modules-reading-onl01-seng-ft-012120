329e597247054849eb2e238b6794010301262482
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-06 21:38:29 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,10 @@
+require_relative './fancy_dance.rb'
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance 
-  extend MetaDancing
+  # include Dance 
+  # extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
   
